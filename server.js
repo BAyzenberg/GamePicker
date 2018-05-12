56d441c7796ce57f3a57efcec50394db36ae806f
Benjamin <ayzenbergb@gmail.com>
2018-05-12 10:29:53 -0700

    added basic command filtering

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const client = new Discord.Client();
 require('dotenv');
 
 client.on('message', message => {
-  if(message.content == 'test') message.reply('response');
+  let object = checkCommand(message);
+  if(object.command == 'test') message.reply('response');
 });
+
+function checkCommand(message) {
+  let split = message.split(' ');
+  let command = split.shift();
+  let content = split.join(' ');
+  return {
+    command,
+    content,
+  };
+}
 
 client.login(process.env.BOT_SECRET);
